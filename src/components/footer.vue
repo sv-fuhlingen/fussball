3faa545d9e5c8e28721dc8cfe2605fe6bbb02ec9
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-11-09 00:15:14 +0100

    init

---
+++ src/components/footer.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+
+const navigationFooter = {
+  social: [
+    {
+
+    },
+  ],
+}
+
+const year = new Date().getFullYear()
+</script>
+
+<template>
+
+
+  <footer class="bg-black" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-12 lg:px-8 mt-12">
+      <div class="grid lg:grid-cols-4 gap-6">
+
+        <div class="mt-8 pt-4 xl:mt-0 text-gray-200 text-base">
+          <h3 class="text-base font-medium text-white ">Vorstand</h3>
+
+          <p class="mt-4 text-gray-400">Michael Schiechel (Vorsitz)</p>
+          <p class="mt-1 text-gray-400">Martina Weitz (stv. Vorsitz)</p>
+
+          <p class="mt-4">Geschäftsadresse:</p>
+
+          <p class="mt-1 text-gray-400">Kasseler Weg 91</p>
+          <p class="mt-1 text-gray-400">50769 Köln</p>
+
+          <p class="mt-4">Fußballplatz:</p>
+          <p class="mt-1 text-gray-400">Am Kutzpfädchen 3a</p>
+          <p class="mt-1 text-gray-400">50769 Köln</p>
+
+        </div>
+
+        <div class="mt-8 pt-4 xl:mt-0 text-gray-200 text-base lg:border-none border-t border-gray-700">
+          <h3 class="text-base font-medium text-white">Geschäftszeiten</h3>
+          <p class="mt-4 text-gray-400">Sie finden uns auf dem Fühlinger Sportplatz zu folgenden Zeiten:</p>
+          <p class="mt-4">Donnerstag: 17-19 Uhr</p>
+        </div>
+
+        <div class="mt-8 pt-4 xl:mt-0 text-gray-200 text-base lg:border-none border-t border-gray-700">
+          <h3 class="text-base font-medium text-white">Über uns</h3>
+          <p class="mt-4 text-gray-400">Der SV Fühlingen-Chorweiler 1929/77 e. V. ist einer der großen und etablierten
+            Vereine im Kölner Norden.</p>
+          <p class="mt-4 text-gray-400">Seit mehr als 90 Jahren aktiv in Köln, bieten wir ein zeitgemäßes Sportangebot
+            an.</p>
+        </div>
+        <div class="mt-8 pt-4 xl:mt-0 text-gray-200 text-base lg:border-none border-t border-gray-700">
+          <h3 class="text-base font-medium text-white">Information</h3>
+          <p class="mt-4 text-gray-400">Landessportbund:</p>
+          <p>LSB-Nr. 200 3012</p>
+
+          <p class="mt-4 text-gray-400">Vereinsregister Köln:</p>
+          <p>VR 6253</p>
+
+          <p class="mt-4 text-gray-400">Steuer-Nr.:</p>
+          <p>217/5962/0600</p>
+
+          <p class="mt-4 text-gray-400">Bankverbindung:</p>
+          <p class="pb-2.5">Volksbank Rhein-Erft-Köln eG</p>
+          <p class="pb-0.5">DE69 3706 2365 4419 1070 13</p>
+          <p>BIC: GENODED1FHH</p>
+        </div>
+      </div>
+      <div class="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
+        <div class="flex space-x-6 md:order-2">
+          <a v-for="item in navigationFooter.social" :key="item.name" :href="item.href"
+             class="text-gray-400 hover:text-gray-300">
+            <span class="sr-only">{{ item.name }}</span>
+            <component :is="item.icon" class="h-6 w-6" aria-hidden="true"/>
+          </a>
+          <a href="#" class="text-gray-400 hover:text-gray-300">
+            Datenschutzerklärung
+          </a>
+          <a href="#" class="text-gray-400 hover:text-gray-300">
+            Impressum
+          </a>
+        </div>
+        <p class="mt-8 text-base text-gray-400 md:order-1 md:mt-0">&copy; {{ year }} <span
+            class="pl-1.5 text-yellow-400">SV Fühlingen-Chorweiler 1929/77 e. V.</span></p>
+      </div>
+    </div>
+  </footer>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/navigation.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import {Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
+import {Bars3Icon, BellIcon, XMarkIcon} from '@heroicons/vue/24/outline'
+
+
+const navigationFooter = {
+  social: [
+    {
+
+    },
+  ],
+}
+
+const TeamsNav =   [
+  {name: 'U12', href: '#'},
+  {name: 'C-Jungend', href: '#'},
+  {name: 'Herren I', href: '#'},
+  {name: 'Herren II', href: '#'},
+  {name: 'Herren III', href: '#'}
+];
+
+let teamsInfo = []
+
+const navigation = [
+  {name: 'Der Verein', href: '#', current: false},
+  {name: 'Probetraining', href: '#', current: false},
+  {
+    name: 'Miglidchaft', href: '#', current: false, data: [
+      {name: 'Probetraing', href: '#'},
+      {name: 'Kundigung', href: '#'},
+    ]
+  },
+  {name: 'Kontakt', href: '#', current: false},
+  {name: 'Sponsorng', href: '#', current: false},
+]
+
+</script>
+<template>
+<Disclosure as="nav" class="bg-black" v-slot="{ open }">
+
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+  <div class="flex h-16 items-center justify-between">
+    <div class="flex items-center">
+      <div class="flex-shrink-5 ">
+        <img class="w-12 mt-6" src="/img/logo.png" alt="SV Fühlingen Logo">
+      </div>
+      <div class="hidden lg:block">
+        <div class="ml-12 flex items-baseline space-x-4">
+          <div v-for="item in navigation">
+            <a v-if="!item.data" :href="item.href"
+               :class="[item.current ? 'text-yellow-400' : 'text-gray-100 hover:bg-yellow-500 hover:text-black', 'px-3 py-2 rounded-md text-sm font-medium']"
+               :aria-current="item.current ? 'page' : undefined">
+              {{ item.name }}
+            </a>
+            <Menu as="div" v-if="item.data" class="relative inline-block text-left">
+              <div>
+                <MenuButton
+                    :class="[item.current ? 'text-yellow-400' : 'text-gray-100 hover:bg-yellow-500 hover:text-black', 'px-3 py-2 rounded-md text-sm font-medium']"
+                    :aria-current="item.current ? 'page' : undefined">
+                  {{ item.name }}
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                       stroke="currentColor" class="w-4 h-4 inline">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                  </svg>
+
+                </MenuButton>
+              </div>
+
+              <transition enter-active-class="transition ease-out duration-100"
+                          enter-from-class="transform opacity-0 scale-95"
+                          enter-to-class="transform opacity-100 scale-100"
+                          leave-active-class="transition ease-in duration-75"
+                          leave-from-class="transform opacity-100 scale-100"
+                          leave-to-class="transform opacity-0 scale-95">
+                <MenuItems
+                    class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-black shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div class="py-1">
+                    <MenuItem v-for="(data, index) in item.data" v-slot="{ active }">
+                      <a href="#"
+                         :class="[
+                                   active ? 'bg-gray-100 text-gray-900' : 'text-gray-300',
+                                   index != 0 ? 'border-t border-t-gray-800' : '',
+                                   'text-gray-100 block px-4 py-2 text-sm hover:bg-black hover:text-yellow-500'
+                                   ]">{{
+                          data.name
+                        }}</a>
+                    </MenuItem>
+                  </div>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="-mr-2 flex lg:hidden">
+      <!-- Mobile menu button -->
+      <DisclosureButton
+          class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+        <span class="sr-only">Open main menu</span>
+        <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true"/>
+        <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true"/>
+      </DisclosureButton>
+    </div>
+  </div>
+</div>
+
+<DisclosurePanel class="lg:hidden">
+  <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+    <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
+                      :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']"
+                      :aria-current="item.current ? 'page' : undefined">{{ item.name }}
+    </DisclosureButton>
+  </div>
+</DisclosurePanel>
+</Disclosure>
+</template>
