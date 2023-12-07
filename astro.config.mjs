70af77299d495d34ae50cd446b4c5313d823dbfa
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-12-07 11:28:30 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-import vercel from "@astrojs/vercel/serverless";
 
 import node from "@astrojs/node";
 
